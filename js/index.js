29ae689bf9588efb28cecd4a02c54ee76ae1129b
briankeegan <brian.morris.keegan@gmail.com>
2018-02-04 17:05:36 -0500

    Update image and fix side-nav
    - New, zoomed in image of mysefl
    - When you click on link in mobile mode, will close `side-nav`

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-$( document ).ready(() => {
+$( document ).ready(function() {
   $(".button-collapse").sideNav();
   $('.parallax').parallax();
   $('.scrollspy').scrollSpy();
+  $('.side-nav > li > a').on('click', () => $('.button-collapse').sideNav('hide'))
 })
