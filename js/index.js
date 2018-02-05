6500a42d1e20b30bb3316d0ae2c88387c51ac8dc
briankeegan <brian.morris.keegan@gmail.com>
2018-02-04 20:02:35 -0500

    Update cards to be sticky with action
    - Using materialize add classes to project cards so:
    - On click will display more info about card
    - Add link placeholders
    - Add some content

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
 $( document ).ready(function() {
-  $(".button-collapse").sideNav();
-  $('.parallax').parallax();
-  $('.scrollspy').scrollSpy();
+  $(".button-collapse").sideNav()
+  $('.parallax').parallax()
+  $('.scrollspy').scrollSpy()
   $('.side-nav > li > a').on('click', () => $('.button-collapse').sideNav('hide'))
+
+  $(window).scroll(() => {
+    // declare variable
+    const top = $(this).scrollTop();
+    const backToTop = $('.back-to-top')
+
+    // if user scrolls down - show scroll to top button
+    if (top > 900) {
+      backToTop.css("opacity", "1")
+
+    } else {
+      backToTop.css("opacity", "0")
+    }
+})
+
+  $('.back-to-top').on('click', () => {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 800) })
+
+
 })
