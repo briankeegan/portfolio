889dd602667ce51b84ad30476da128577bc3f23f
briankeegan <brian.morris.keegan@gmail.com>
2018-03-14 14:51:25 -0400

    Update colors, and mobile support for About

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ const backToTop = () => {
 $( document ).ready(function() {
   $(".button-collapse").sideNav()
   $('.parallax').parallax()
-  $('.scrollspy').scrollSpy()
   $('.side-nav > li > a').on('click', () => $('.button-collapse').sideNav('hide'))
 
   $(window).scroll(() => {
@@ -21,7 +20,31 @@ $( document ).ready(function() {
     } else {
       backToTop.css("opacity", "0")
     }
-})
+  })
+
+  $('.about-click').click(function () {
+    $('html, body').animate({
+      scrollTop: $('#about').offset().top - 64
+    }, 1000)
+  })
+
+  $('.technologies-click').click(function () {
+    $('html, body').animate({
+      scrollTop: $('#technologies').offset().top - 64
+    }, 1000)
+  })
+
+  $('.portfolio-click').click(function () {
+    $('html, body').animate({
+      scrollTop: $('#portfolio').offset().top - 64
+    }, 1000)
+  })
+
+  $('.contact-click').click(function () {
+    $('html, body').animate({
+      scrollTop: $('#contact').offset().top - 64
+    }, 1000)
+  })
 
   $('.back-to-top').on('click', backToTop)
   $('.back-to-top').on('click', backToTop)
